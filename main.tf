609a5b21c38a4df20d3a4736d6bc906815b9a6c0
Allan Taylor <allan@startmail.com>
2021-11-05 14:27:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,19 +13,19 @@ resource "null_resource" "deploy_istio" {
     command = <<-EOT
       curl -L https://istio.io/downloadIstio | ISTIO_VERSION=${var.istio_version} TARGET_ARCH=x86_64 sh 
       cd istio-${var.istio_version}
-      kubectl create namespace istio-system
       
       # Install the Istio base chart
-      helm install istio-base manifests/charts/base -n istio-system
+      helm install istio-base manifests/charts/base --values ../terraform-kubernetes-istio/base-values.yaml --namespace istio-system
         
       # Install the Istio discovery chart
-      helm install istiod manifests/charts/istio-control/istio-discovery -n istio-system
+      helm install istiod manifests/charts/istio-control/istio-discovery --values ../terraform-kubernetes-istio/discovery-values.yaml --namespace istio-system 
       
       # Install the Istio ingress gateway chart
-      helm install istio-ingress manifests/charts/gateways/istio-ingress -n istio-system
+      helm install istio-ingress manifests/charts/gateways/istio-ingress --values ../terraform-kubernetes-istio/ingress-gw-values.yaml --namespace istio-system
       
       # Install the Istio egress gateway chart
-      helm install istio-egress manifests/charts/gateways/istio-egress -n istio-system
+      helm install istio-egress manifests/charts/gateways/istio-egress --values ../terraform-kubernetes-istio/egress-gw-values.yaml --namespace istio-system
     EOT
   }
 }
+
