0f1a5ad302cd0a89c8f18d4960e8c99cef7fd198
Allan Taylor <allan@startmail.com>
2021-11-05 20:37:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,17 +15,35 @@ resource "null_resource" "deploy_istio" {
       cd istio-${var.istio_version}
       
       # Install the Istio base chart
-      helm install istio-base manifests/charts/base --values ../terraform-kubernetes-istio/base-values.yaml --namespace istio-system
+      helm install istio-base manifests/charts/base \
+        --values ../terraform-kubernetes-istio/chart-values/base-values.yaml \
+        --namespace istio-system
         
       # Install the Istio discovery chart
-      helm install istiod manifests/charts/istio-control/istio-discovery --values ../terraform-kubernetes-istio/discovery-values.yaml --namespace istio-system 
+      helm install istiod manifests/charts/istio-control/istio-discovery \
+        --values ../terraform-kubernetes-istio/chart-values/discovery-values.yaml \
+        --namespace istio-system 
       
       # Install the Istio ingress gateway chart
-      helm install istio-ingress manifests/charts/gateways/istio-ingress --values ../terraform-kubernetes-istio/ingress-gw-values.yaml --namespace istio-system
+      helm install istio-ingress manifests/charts/gateways/istio-ingress \
+        --values ../terraform-kubernetes-istio/chart-values/ingress-gw-values.yaml \
+        --namespace istio-system
       
       # Install the Istio egress gateway chart
-      helm install istio-egress manifests/charts/gateways/istio-egress --values ../terraform-kubernetes-istio/egress-gw-values.yaml --namespace istio-system
+      helm install istio-egress manifests/charts/gateways/istio-egress \
+        --values ../terraform-kubernetes-istio/chart-values/egress-gw-values.yaml \
+        --namespace istio-system
     EOT
   }
 }
 
+resource "null_resource" "deploy_bookinfo" {
+  count = var.bookinfo_sample_app ? enabled : 0
+  depends_on = [null_resource.setup_env]
+  provisioner "local-exec" {
+    command = <<-EOT
+      cd istio-${var.istio_version}
+      kubectl -n bookinfo apply -f samples/bookinfo/platform/kube/bookinfo.yaml
+    EOT
+  }
+}
