e9a44a6dbcd21f264d90a33bd06a82da80e8856d
Allan Taylor <allan@startmail.com>
2021-11-06 10:35:23 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "istio_version" {
     type = string
     description = "The version of Istio to deploy."
 }
+
+variable "bookinfo_sample_app" {
+    type = bool
+    description = "If enabled this will deploy the book info sample micro service."
+}
+
+variable "kiali_mgmt_console" {
+    type = bool
+    description = "If enabled this will deploy a management console for Istio service mesh."
+}
