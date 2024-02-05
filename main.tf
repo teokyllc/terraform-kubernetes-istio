e66e93a2e834b6ab546c93a064be6eee4082f241
Allan Taylor <allan@startmail.com>
2024-02-05 08:14:29 -0500

    wip

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "helm_release" "ingress_gateway" {
   repository = "https://istio-release.storage.googleapis.com/charts"
   chart      = "gateway"
   version    = var.istio_version
-  namespace  = "istio-system"
+  namespace  = var.istio_namespace
 
   set {
     name  = "replicaCount"
@@ -52,5 +52,36 @@ resource "helm_release" "ingress_gateway" {
   set {
     name  = "autoscaling.minReplicas"
     value = var.ingress_gateway_replica_count
+  }
+}
+
+resource "kubernetes_manifest" "tls_certificate" {
+  count      = var.create_tls_certificate ? 1 : 0
+  depends_on = [helm_release.ingress_gateway]
+  manifest   = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "Certificate"
+    "metadata"   = {
+      "name"      = var.tls_certificate_name
+      "namespace" = var.istio_namespace
+    }
+    "spec" = {
+      "dnsNames"  = var.certificate_dns_names
+      "issuerRef" = {
+        "group" = "cert-manager.io"
+        "kind"  = "ClusterIssuer"
+        "name"  = var.cluster_issuer_name
+      }
+      "secretName" = var.tls_secret_name
+      "renewBefore" = "360h0m0s"
+      "usages"     = [
+        "digital signature",
+        "key encipherment",
+      ]
+    }
+  }
+
+  field_manager {
+    force_conflicts = true
   }
 }
--- variables.tf
+++ variables.tf
@@ -21,3 +21,33 @@ variable "ingress_gateway_replica_count" {
   description = "The number of replicas for the istio ingress gateway pod."
   default     = null
 }
+
+variable "create_tls_certificate" {
+  type        = bool
+  description = "If true, creates a TLS certificate in the istio namespace."
+  default     = false
+}
+
+variable "tls_certificate_name" {
+  type        = string
+  description = "The name of the cert-manager TLS certificate."
+  default     = null
+}
+
+variable "certificate_dns_names" {
+  type        = list(any)
+  description = "A list of DNS names for the TLS certificate."
+  default     = null
+}
+
+variable "cluster_issuer_name" {
+  type        = string
+  description = "The name of the cert-manager ClusterIssuer."
+  default     = null
+}
+
+variable "tls_secret_name" {
+  type        = string
+  description = "The name of the certificate underlying secret."
+  default     = null
+}
