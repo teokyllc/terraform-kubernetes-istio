aaacc8c02863d745862ef597940492a7787b0239
Allan Taylor <allan@startmail.com>
2024-02-05 08:31:30 -0500

    wip

--- data.tf
+++ data.tf
@@ -4,8 +4,8 @@ resource "time_sleep" "wait_30_seconds" {
 }
 
 
-data "kubernetes_service" "istio_ingress_gateway" {
-  depends_on = [time_sleep.wait_30_seconds]
+data "kubernetes_service_v1" "istio_ingress_gateway" {
+  depends_on = [time_sleep.wait_30_seconds, helm_release.ingress_gateway]
   metadata {
     name      = "ingress-gateway"
     namespace = "istio-system"
